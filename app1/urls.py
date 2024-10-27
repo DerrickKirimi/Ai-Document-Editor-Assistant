8ff2b701d3c58634ceab36a39e55106e0a66c47a
DerrickKirimi <derrickkiriimi@gmail.com>
2024-10-28 00:01:48 +0300

    Add urls for authentication

--- app1/urls.py
+++ app1/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LoginView, LogoutView
 
+from .views import (main, register, logout_user)
 urlpatterns = [
     path('', views.main, name='main'),
+    path("accounts/login/", LoginView.as_view(template_name="app1/login.html"),
+        name="login"),
+    path("logout/", LogoutView.as_view(template_name="app1/logout.html"),
+        name="logout"),
+    path("register/", register, name="register"),
     path('UPLOAD', views.upload_document, name='upload_document'),
     path('summary', views.summarization, name='summary'),
 ]
