bfd750258c8fa9fd17806bad50a9031b006f336e
DerrickKirimi <derrickkiriimi@gmail.com>
2024-10-31 22:53:56 +0300

    Make content editable in admin

--- document_assistant/admin.py
+++ document_assistant/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Document, Content
 
-# Register your models here.
+class ContentInline(admin.StackedInline):
+    model = Content
+    fields = ('original_text', 'improved_text')
+    extra = 0  # No extra blank fields by default
+
+@admin.register(Document)
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'upload_date', 'status', 'title')
+    search_fields = ('user__username', 'status', 'title')
+    list_filter = ('status', 'upload_date')
+    ordering = ('-upload_date',)
+    inlines = [ContentInline]  # Add the Content inline to Document admin
+
+    # Specify editable fields
+    fields = ('user', 'title', 'status')  # Only these fields will appear for editing in Document
+
